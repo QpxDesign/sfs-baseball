63c0aee29cf46dfd9e59f82c365f4836b407de18
qpxdesign <qroshan5@gmail.com>
2022-04-04 18:45:05 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./logo.png"; // with import
 
+function toggleDropdown() {
+  if (document.getElementById("dropdownMenu-wrapper").style.display == "none") {
+    document.getElementById("dropdownMenu-wrapper").style.display = "flex";
+  } else {
+    document.getElementById("dropdownMenu-wrapper").style.display = "none";
+  }
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="header">
+        <img src={logo} alt="sfs baseball"></img>
+        <nav className="navbar-wrapper">
+          <div className="navbar-item item-1">Varsity</div>
+          <div className="navbar-item item-2">J.V.</div>
+          <div className="navbar-item item-3">Flordia</div>
+          <div className="navbar-item item-4">Records</div>
+          <div className="navbar-item item-5">History</div>
+          <div className="navbar-item item-6">Info</div>
+        </nav>
+        <div id="dropdownMenu-toggle" onClick={toggleDropdown}>
+          =
+        </div>
       </header>
-    </div>
+      <div id="dropdownMenu-wrapper">
+        <div className="dropdownMenu-item dropdownitem-1">Varsity</div>
+        <div className="dropdownMenu-item dropdownitem-2">J.V.</div>
+        <div className="dropdownMenu-item dropdownitem-3">Flordia</div>
+        <div className="dropdownMenu-item dropdownitem-4">Records</div>
+        <div className="dropdownMenu-item dropdownitem-5">History</div>
+        <div className="dropdownMenu-item dropdownitem-6">Info</div>
+      </div>
+      <div className="featuredPages-wrapper">
+        <div className="featuredPage girditem-1">
+          <h1>Upcoming Games</h1>
+          <div></div>
+        </div>
+        <div className="featuredPage girditem-2"></div>
+        <div className="featuredPage girditem-3"></div>
+        <div className="featuredPage girditem-4"></div>
+        <div className="featuredPage girditem-5"></div>
+        <div className="featuredPage girditem-6"></div>
+        <div className="featuredPage girditem-7"></div>
+        <div className="featuredPage girditem-8"></div>
+        <div className="featuredPage girditem-9"></div>
+      </div>
+    </>
   );
 }
 
